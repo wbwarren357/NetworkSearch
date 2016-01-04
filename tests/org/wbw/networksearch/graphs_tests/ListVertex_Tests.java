af16d43151a70b7c3d4afd291995598a38c458c2
Will Warren <wbwarren@alum.mit.edu>
2016-01-03 19:30:28 -0500

    Completed Junit tests for ListVertex

--- tests/org/wbw/networksearch/graphs_tests/ListVertex_Tests.java
+++ tests/org/wbw/networksearch/graphs_tests/ListVertex_Tests.java
@@ -3,6 +3,9 @@ package org.wbw.networksearch.graphs_tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.wbw.networksearch.abstractgraphs.AbstractTransformVcoords;
@@ -15,45 +18,50 @@ public class ListVertex_Tests {
 
 	private IntegerVcoordinates integerVcoordinates = new IntegerVcoordinates();
 	private ListStringifyVcoords<Integer> listStringifyVcoords = new ListStringifyVcoords<Integer>();
-
+	private List<Integer> listOf2 = new ArrayList<Integer>();
+	private List<Integer> listOf5 = new ArrayList<Integer>();
+	
 	@Before
 	public void setUp() {
 		listStringifyVcoords.setVcoordTransformer((AbstractTransformVcoords<Integer>)integerVcoordinates);
 		ListVertex<Integer> lv = new ListVertex<Integer>();
 		lv.setStringifyVcoords(listStringifyVcoords);
+
+		listOf2.add(new Integer(-1));
+		listOf2.add(new Integer(2));
+
+		listOf5.add(new Integer(1));
+		listOf5.add(new Integer(2));
+		listOf5.add(new Integer(3));
+		listOf5.add(new Integer(4));
+		listOf5.add(new Integer(5));
+
 	}
 	
 	@Test
-	public void testListVertex_getVname() {
-		ListVertex<Integer> lv = new ListVertex(VertexType.IS_REAL, "1,4");
-		assertEquals(lv.getVname(), "1.4");
-		
-		
+	public void testVcoordsConstructor() {
+		ListVertex<Integer> lv = new ListVertex<Integer>(VertexType.IS_REAL, listOf2);
+		assertEquals(lv.getVname(), "-1,2");
+		assertEquals(lv.getVcoordinates(), listOf2);
+		assertEquals(lv.getVtypel(), VertexType.IS_REAL);
+
+		lv = new ListVertex<Integer>(VertexType.IS_VIRTUAL, listOf5);
+		assertEquals(lv.getVname(), "1,2,3,4,5");
+		assertEquals(lv.getVcoordinates(), listOf5);
+		assertEquals(lv.getVtypel(), VertexType.IS_VIRTUAL);
 	}
 
-//	@Test
-//	public void testListVertexVertexTypeString() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetVname() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetVcoordinates() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetVtype() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetVtypel() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	public void testVnameConstructor() {
+		ListVertex<Integer> lv = new ListVertex<Integer>(VertexType.IS_REAL, "-1,2");
+		assertEquals(lv.getVname(), "-1,2");
+		assertEquals(lv.getVcoordinates(), listOf2);
+		assertEquals(lv.getVtypel(), VertexType.IS_REAL);
 
+		System.out.println("Test 4");
+		lv = new ListVertex<Integer>(VertexType.IS_VIRTUAL, "1,2,3,4,5");
+		assertEquals(lv.getVname(), "1,2,3,4,5");
+		assertEquals(lv.getVcoordinates(), listOf5);
+		assertEquals(lv.getVtypel(), VertexType.IS_VIRTUAL);
+	}
 }
